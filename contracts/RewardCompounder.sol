1c150795f554ec66131f9eaddd00a0cd11470698
CalabashSquash <my_name_uhh@protonmail.com>
2022-11-29 13:58:09 +1000

    Natspecs and sell function variable names

--- contracts/RewardCompounder.sol
+++ contracts/RewardCompounder.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import '@openzeppelin/contracts/access/Ownable2Step.sol';
-import {IRewardCompounder, IRewardTracker, ISellingStrategy} from './interfaces.sol';
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {IRewardCompounder, IRewardTracker, ISellingStrategy} from "./interfaces.sol";
 
+/**
+ * @title Compound WETH rewards from MYC staking.
+ * @author iflp
+ * @author CalabashSquash
+ */
 contract RewardCompounder is IRewardCompounder, Ownable2Step {
     IRewardTracker immutable REWARD_TRACKER;
     address immutable WETH;
@@ -11,7 +16,14 @@ contract RewardCompounder is IRewardCompounder, Ownable2Step {
 
     address public strategy;
 
-    constructor(address weth, address myc, address rewardTracker) {
+    /**
+     * @notice Sets storage variables and approves rewardTracker to spend MYC (for staking).
+     */
+    constructor(
+        address weth,
+        address myc,
+        address rewardTracker
+    ) {
         WETH = weth;
         MYC = myc;
         REWARD_TRACKER = IRewardTracker(rewardTracker);
@@ -19,33 +31,54 @@ contract RewardCompounder is IRewardCompounder, Ownable2Step {
     }
 
     /**
-    * Compounds rewards for msg.sender
-    */
+     * @notice Compounds rewards for msg.sender.
+     */
     function compound() external {
-        uint256 wethAmount = REWARD_TRACKER.claimForAccount(msg.sender, address(this));
+        uint256 wethAmount = REWARD_TRACKER.claimForAccount(
+            msg.sender,
+            address(this)
+        );
 
         if (wethAmount != 0) {
             (bool success, bytes memory data) = strategy.delegatecall(
-                abi.encodeWithSignature("sell(uint256,address,address)", wethAmount, WETH, MYC)
+                abi.encodeWithSignature(
+                    "sell(uint256,address,address)",
+                    wethAmount,
+                    WETH,
+                    MYC
+                )
             );
 
             if (!success) revert CompoundFailed();
             uint256 mycAmount = abi.decode(data, (uint256));
-            REWARD_TRACKER.stakeForAccount(address(this), msg.sender, MYC, mycAmount);
+            REWARD_TRACKER.stakeForAccount(
+                address(this),
+                msg.sender,
+                MYC,
+                mycAmount
+            );
 
             emit Compounded(msg.sender, wethAmount, mycAmount);
         }
     }
 
+    /**
+     * @notice Returns true iff given address is a contract.
+     */
     function _isContract(address addr) private view returns (bool) {
-    uint size;
-    assembly { size := extcodesize(addr) }
-    return size > 0;
+        uint256 size;
+        assembly {
+            size := extcodesize(addr)
+        }
+        return size > 0;
     }
 
-    function setStrategy(address _newStrategy) external onlyOwner {
-        if (_isContract(_newStrategy) == false) revert NotContract();
-        emit StrategyUpdated(_newStrategy, strategy); 
-        strategy = _newStrategy;
+    /**
+     * @notice Set the WETH selling strategy contract.
+     */
+    function setStrategy(address newStrategy) external onlyOwner {
+        if (_isContract(newStrategy) == false) revert NotContract();
+        emit StrategyUpdated(newStrategy, strategy);
+        strategy = newStrategy;
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,36 @@
 import * as dotenv from 'dotenv';
 import { HardhatUserConfig } from 'hardhat/config';
+import "hardhat-gas-reporter"
 import '@nomicfoundation/hardhat-toolbox';
 dotenv.config();
 
+// The test private keys are well-known and used as an example only
+const PRIVATE_KEY = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
+
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-      {
-        version: `0.8.17`,
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 1000
-          }
-        }
-      }
-    ]
-  },
-  networks: {
-    arbitrum: {
-      url: process.env.ARB_RPC ?? ``,
-      accounts: [process.env.ARB_PRIVATE_KEY ?? ``]
+    solidity: {
+        compilers: [
+            {
+                version: `0.8.17`,
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000
+                    }
+                }
+            }
+        ]
     },
-    arbitest: {
-      url: process.env.ARB_TESTNET_RPC ?? ``,
-      accounts: [process.env.ARB_TESTNET_PRIVATE_KEY ?? ``]
+    networks: {
+        arbitrum: {
+            url: process.env.ARB_RPC ?? ``,
+            accounts: [process.env.ARB_PRIVATE_KEY ?? PRIVATE_KEY]
+        },
+        arbitest: {
+            url: process.env.ARB_TESTNET_RPC ?? ``,
+            accounts: [process.env.ARB_TESTNET_PRIVATE_KEY ?? PRIVATE_KEY]
+        }
     }
-  }
 };
 
 export default config;
